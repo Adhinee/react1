d664e634a982b3d39cfb3a8a111055eb407f056f
adhinee <[mohammedadhinee@gmail.com]>
2024-07-24 22:43:55 +0530

    commit

--- src/AddItem.js
+++ src/AddItem.js
@@ -9,7 +9,7 @@ const AddItem = ({newItem,setNewItem,handleSubmit}) => {
             autoFocus
             id='addItem'
             type="text"
-            placeholder='Add Item'
+            placeholder="Don't Add Item"
             value={newItem}
             onChange={(e) => setNewItem(e.target.value)}
             required />
